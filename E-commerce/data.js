271769a682c73f616a0a2eb47a9f9b481bb7319c
jgvegarivera <jgvega@vivaorganica.com.mx>
2020-10-27 21:01:57 -0700

    Practica 14 y crear cuenta e-commerce

---
+++ E-commerce/data.js
@@ -0,0 +1,15 @@
+//Variables
+let rndNumber = Math.random().toString(36).substr(1,4);
+
+let data = {
+    email: 'dummy' + rndNumber + '@mailinator.com',
+    firstName: "Gabo",
+    lastName: "Rivery",
+    password: "12345678",
+    pbox: "PO Box 533",
+    city: "Sinaloa",
+    zip: "35050",
+    mobile: "6673000000",
+    adress_Alias: "My address"
+}
+export{data}
